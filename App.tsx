a3f34c0bf0f5947f83ccd2bd9249b1d1a6cb5b55
Francisco Fanton <francisco.fanton@4rsoluciones.com>
2023-06-27 11:52:08 -0300

    add main tabs

--- App.tsx
+++ App.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/Home';
 import UserScreen from './src/screens/User';
+import MainTabs from './src/navigators/main-tabs';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName={'MainTabs'}
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="User" component={UserScreen} />
+        <Stack.Screen name={'MainTabs'} component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
